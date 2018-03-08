228171c17eabac5ef5d64648400033a81cb98e59
Mohammed Hameed <mohammed.hameed@email.com>
2018-03-08 15:13:25 -0600

    added variables for player names and if statement for player name assignment

--- js/logic.js
+++ js/logic.js
@@ -1,17 +1,31 @@
-$(document).ready(function () {
-    //setup and initialize firebase database
+$(document).ready(function() {
+    // Initialize Firebase
     var config = {
         apiKey: "AIzaSyAuye1u9w6lqvR92y2QlK89pSd3lKSW1ho",
         authDomain: "sample-976ac.firebaseapp.com",
         databaseURL: "https://sample-976ac.firebaseio.com",
         projectId: "sample-976ac",
-        storageBucket: "",
+        storageBucket: "sample-976ac.appspot.com",
         messagingSenderId: "21091739234"
     };
     firebase.initializeApp(config);
 
     var database = firebase.database();
-    var ref = database.ref();
+    //database.ref();
 
+    //initial variables
+    var player1 = "",
+        player2 = "";
 
-})
+    $("#submit").on("click", function(){
+        if (player1 === ""){
+            player1 = $("#playerName").val();
+            console.log("p1: " +player1);
+        
+        } else {
+            player2 = $("#playerName").val();
+            console.log("p2: "+player2);
+        };
+
+    });
+});
