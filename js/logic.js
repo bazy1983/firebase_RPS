9e82d5fb8742f5bd55753d5e057da970c387e04b
Mohammed Hameed <mohammed.hameed@email.com>
2018-03-09 01:06:25 -0600

    bulit unique session for each window to identify each player

--- js/logic.js
+++ js/logic.js
@@ -16,39 +16,69 @@ $(document).ready(function() {
     //initial variables
     var player1 = "",
         player2 = "",
-        tracker = 0;// track which player on document
+        playerID;// track which player on document
 
     $("#submit").on("click", function(){
-        if (player1 === ""){ //set player 1 data
+        console.log ("player on this window: " + playerID)
+        if (playerID === 0){ //set player 1 data
             player1 = $("#playerName").val();
-            var ref = database.ref("player/1");
+            var ref = database.ref("player/0");
             ref.set({
                 name: player1,
                 win: 0,
                 lose: 0
             });
-        
-        } else { //set player 2 data
+            hide()
+            sessionStorage.setItem("playerID", 0);
+
+        } else if (playerID === 1) { //set player 2 data
             player2 = $("#playerName").val();
-            var ref = database.ref("player/2");
+            var ref = database.ref("player/1");
             ref.set({
                 name: player2,
                 win: 0,
                 lose: 0
             });
+            hide();
+            sessionStorage.setItem("playerID", 1); 
         };
 
     });
+
+    function hide (){
+        $("#submit").hide();
+        $("label").hide();
+        $("#playerName").hide();
+    }
+
     // when data change in database
     database.ref("player").on("value", response, error)
 
     function response(res) {
         console.log(res.val());
+        //sets which player on window
+        if (res.val() === null){
+            playerID = 0;
+        } else {
+            playerID = 1
+        }
+        
     };
 
     function error(err){
         console.log(err)
     }
 
+    //clears player information when navigate away from window
+    $(window).on("unload", function(){
+        database.ref("player/"+ sessionStorage.getItem("playerID")).remove();
+    })
+
+
+$("#console").click (function(){
+    console.log(sessionStorage.getItem("playerID"))
+})
+
 
 });
+
