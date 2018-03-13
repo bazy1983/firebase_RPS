26481fd5e793e77f222d482de324d91937973da3
Mohammed Hameed <mohammed.hameed@email.com>
2018-03-13 00:44:36 -0500

    added chat functionality

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyAuye1u9w6lqvR92y2QlK89pSd3lKSW1ho",
@@ -11,24 +11,29 @@ $(document).ready(function() {
     firebase.initializeApp(config);
 
     var database = firebase.database();
-    
+
+    //clear stored chat
+    database.ref("chat").remove();
+
 
     //initial variables
     var player1 = "",
         player2 = "",
         playerID;// track which player on document
 
-    $("#submit").on("click", function(){
-        
-        if (playerID === 0){ //set player 1 data
+    $("#submit").on("click", function () {
+
+        if (playerID === 0) { //set player 1 data
             player1 = $("#playerName").val();
             var ref = database.ref("player/" + playerID);
             ref.set({
                 name: player1,
                 win: 0,
-                lose: 0
+                lose: 0,
+                choice: ""
             });
             sessionStorage.setItem("playerID", 0);
+            sessionStorage.setItem("name", player1);
             hide()
 
         } else if (playerID === 1) { //set player 2 data
@@ -37,21 +42,25 @@ $(document).ready(function() {
             ref.set({
                 name: player2,
                 win: 0,
-                lose: 0
+                lose: 0,
+                choice: ""
             });
-            sessionStorage.setItem("playerID", 1); 
+            sessionStorage.setItem("playerID", 1);
+            sessionStorage.setItem("name", player2);
+
+
             hide();
         };
 
     });
 
-    function hide (){
+    function hide() {
         $("#submit").fadeOut();
         $("label").fadeOut();
         $("#playerName").fadeOut();
     }
 
-    
+
 
     // when data change in database
     database.ref("player").on("value", response, error)
@@ -62,48 +71,95 @@ $(document).ready(function() {
 
         // console.log(res.val());
         //sets which player on window
-        if (res.child(0).exists()){
+        if (res.child(0).exists()) {
             playerID = 1;
         } else {
-            playerID =0
+            playerID = 0
         }
 
 
-        if (res.child(0).exists()){
+        if (res.child(0).exists()) {
             $(".p0").text(res.val()[0].name);
+            $("#player0 i").removeClass("hidden");
         } else {
             $(".p0").text("PLAYER");
         }
 
-        if(res.child(1).exists()) {
+        //shows player information on html
+        if (res.child(1).exists()) {
             $(".p1").text(res.val()[1].name);
+            $("#player1 i").removeClass("hidden");
         } else {
             $(".p1").text("PLAYER");
         }
-        
-        
+
+        if(res.child(0).exists() && res.child(1).exists()){
+            $(".chatbox").removeClass("disabled")
+        } else {
+            $(".chatbox").addClass("disabled")
+        }
+
 
     };
 
 
 
-    function error(err){
+    function error(err) {
         console.log(err)
     }
 
+
+    $("#player0 i").on("click", function () {
+        console.log($(this).attr("data"))
+        $(this).siblings().addClass("disabled");
+    })
+
+    
+    
+    $("#console").click(function () {
+        console.log("player ID is: " + playerID)
+    })
+    
+    
+    //chat functionality
+    $("#chat").on("click", function () {
+        var StoredName = sessionStorage.getItem("name");
+        database.ref("chat").set({
+            name: StoredName,
+            message: $("#chatinput").val().trim()
+        });
+        $("#chatinput").empty();
+    })
+    
+    database.ref("chat").on("value", chatResponse)
+    
+    function chatResponse(res) {
+        if(res.child("name").exists()){
+        chatData = res.val();
+        var newP = $("<p class= 'mychat'>"),
+            spanName = $("<span class = 'font-weight-bold'>").text(chatData.name+ ": "),
+            spanChat = $("<span>").text(chatData.message);
+            newP.append(spanName, spanChat);
+            $(".room").append(newP);    
+        }
+    }
+    
+    
     //clears player information when navigate away from window
-    $(window).on("unload", function(){
+    $(window).on("unload", function () {
         var playerIndex = sessionStorage.getItem("playerID");
-        database.ref("player/"+ playerIndex).remove();
+        var StoredName = sessionStorage.getItem("name");
+        var newP = $("<p class= 'mychat'>"),
+            spanChat = $("<span>").text(StoredName + " Has Disconnected");
+            newP.append(spanChat);
+            $(".room").append(newP);  
+        database.ref("player/" + playerIndex).remove();
+
         sessionStorage.clear();
     })
 
 
-$("#console").click (function(){
-    console.log("player ID is: " + playerID)
-})
-
-
-
 });
+
+
 
